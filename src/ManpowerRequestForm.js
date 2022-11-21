eba6b740b691d970904cf4c4a5436ba444494c7a
YoungMW <shawn.wayne.jcrc12@gmail.com>
2022-11-21 15:48:10 +0800

    updated inputs

--- src/ManpowerRequestForm.js
+++ src/ManpowerRequestForm.js
@@ -5,6 +5,29 @@ const ManpowerRequestForm = () => {
   const [date, setDate] = useState("");
   const [mainContractor, setMainContractor] = useState("");
   const [pic, setPIC] = useState("");
+  const [jobItem, setJobItem] = useState("");
+  const [location, setLocation] = useState("");
+  const [workers, setWorkers] = useState([]);
+  const [requestArray, setRequestArray] = useState([]);
+
+  const handleAddRequest = () => {
+    const array = [
+      ...requestArray,
+      { job_Item: jobItem, location: location, workers: workers },
+    ];
+    setRequestArray(array);
+    clearInput();
+  };
+
+  console.log(requestArray);
+
+  //--------------------clear input field---------------------
+  function clearInput() {
+    var getValue = document.getElementById("valueID");
+    if (getValue.value != "") {
+      getValue.value = "";
+    }
+  }
 
   return (
     <div className="create--request--container">
@@ -66,6 +89,91 @@ const ManpowerRequestForm = () => {
             />
           </div>
         </div>
+        <span className="fs16 fw700 white">Request:</span>
+        {requestArray.map((details) => {
+          return (
+            <div className="add--service--input--container mt8 mb24">
+              <span className="fs16 fw700 black mb8 fs24 fw700">
+                Request {requestArray.length}
+              </span>
+              <span className="fs16 fw700 black mb8 fs12 fw700">Job item:</span>
+              <div className="add--service--input--forms--full mb8">
+                <div className="created--request--input">
+                  {details.job_Item}
+                </div>
+              </div>
+              <span className="fs16 fw700 black mb8 fs12 fw700">Location:</span>
+              <div className="add--service--input--forms--full mb8">
+                <div className="created--request--input">
+                  {details.job_Item}
+                </div>
+              </div>
+              <span className="fs16 fw700 black mb8 fs12 fw700">
+                Worker's Name:
+              </span>
+              <div className="add--service--input--forms--full mb8">
+                <div className="created--request--input">
+                  {details.job_Item}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="add--service--input--container mt8 mb24">
+          <span className="fs16 fw700 black mb8 fs24 fw700">Add Request</span>
+          <span className="fs16 fw700 black mb8 fs12 fw700">Job item:</span>
+          <div className="add--service--input--forms--full mb8">
+            <input
+              type="text"
+              placeholder="Job item"
+              className="create--request--input"
+              value={jobItem}
+              id="valueID"
+              onChange={(e) => {
+                setJobItem(e.target.value);
+              }}
+            />
+          </div>
+          <span className="fs16 fw700 black mb8 fs12 fw700">Location:</span>
+          <div className="add--service--input--forms--full mb8">
+            <input
+              type="text"
+              placeholder="Location"
+              value={location}
+              className="create--request--input"
+              id="valueID"
+              onChange={(e) => {
+                setLocation(e.target.value);
+              }}
+            />
+          </div>
+          <span className="fs16 fw700 black mb8 fs12 fw700">
+            Worker's Name:
+          </span>
+          <div className="add--service--input--forms--full mb8">
+            <input
+              type="text"
+              placeholder="e.g Safiku"
+              value={workers}
+              className="create--request--input"
+              id="valueID"
+              onChange={(e) => {
+                setWorkers(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <button
+          className="add--request--button mb8 white"
+          onClick={() => {
+            handleAddRequest();
+            setJobItem(null);
+            setLocation(null);
+            setWorkers(null);
+          }}
+        >
+          Add Request
+        </button>
       </div>
     </div>
   );
